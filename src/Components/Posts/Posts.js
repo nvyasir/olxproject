ce54648b860d156289f7c7fe9b8b5f7b8e9d4a24
nvyasir <nvyasir@gmail.com>
2021-09-14 23:29:27 +0400

    Listed added products in post area.

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -17,7 +17,9 @@ function Posts() {
             
           }
         })
-        console.log(allPost);
+        
+        setProducts(allPost)
+        
       })
 
     }, [])
@@ -28,25 +30,37 @@ return (
           <span>Quick Menu</span>
           <span>View more</span>
         </div>
+        
         <div className="cards">
-          <div
-            className="card"
-          >
+          
+         {products.map(product=>{
+           console.log(product);
+           return(
+             
+
+           <div className="card">
             <div className="favorite">
               <Heart></Heart>
             </div>
             <div className="image">
-              <img src="../../../Images/R15V3.jpg" alt="" />
+              <img src={product.url} alt={product.item} />
             </div>
             <div className="content">
-              <p className="rate">&#x20B9; 250000</p>
-              <span className="kilometer">Two Wheeler</span>
-              <p className="name"> YAMAHA R15V3</p>
+              <p className="rate">&#x20B9; {product.price}</p>
+              <span className="kilometer">{product.item}</span>
+              <p className="name"> {product.category}</p>
             </div>
             <div className="date">
-              <span>Tue May 04 2021</span>
+              <span>{product.createdAt}</span>
             </div>
           </div>
+        
+
+        ) })
+         } 
+          
+
+          
         </div>
       </div>
       <div className="recommendations">
